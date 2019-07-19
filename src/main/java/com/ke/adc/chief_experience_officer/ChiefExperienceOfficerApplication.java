170a236726f7ceb614afb80fb2a9ef9c2fb96da3
bysoul <bysoulwarden@outlook.com>
2019-07-19 20:31:54 +0800

    init

---
+++ src/main/java/com/ke/adc/chief_experience_officer/ChiefExperienceOfficerApplication.java
@@ -0,0 +1,13 @@
+package com.ke.adc.chief_experience_officer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiefExperienceOfficerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiefExperienceOfficerApplication.class, args);
+    }
+
+}
